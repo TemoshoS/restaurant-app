f01cdf32413b1b95fc31f9e3c6d75ef603c8edbd
TemoshoS <temoshomaduane@gmail.com>
2023-10-30 07:39:22 +0200

    commit

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
 
   function MainStack() {
     return (
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName='Restaurants'>
         <Stack.Screen name="Restaurants" component={RestaurantScreen} options={{ headerShown: false }} />
         <Stack.Screen name='Welcome' component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false }} />
